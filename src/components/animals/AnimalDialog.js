ae577dbea57f623d6179c57cc3e569264c5ca342
Jubjub422 <ross.stovenour@gmail.com>
2021-11-30 15:19:14 -0600

    do not use

--- src/components/animals/AnimalDialog.js
+++ src/components/animals/AnimalDialog.js
@@ -1,17 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
+import AnimalRepository from "../../repositories/AnimalRepository"
 
-export const AnimalDialog = ({toggleDialog, animal}) => {
+
+export const AnimalDialog = ({ toggleDialog, animal, setCurrentAnimal }) => {
+    const [treatment, setTreatment] = useState("")
     return (
         <dialog id="dialog--animal" className="dialog--animal">
-            <h2 style={{ marginBottom: "1.3em" }}>Medical History for {animal.name}</h2>
+            <h2 style={{ marginBottom: "1.3em" }}>Medical History for {animal?.name}</h2>
             {
-                animal.treatments.map(t => (
+                animal?.treatments?.map(t => (
                     <div key={t.id}>
                         <h4>{new Date(t.timestamp).toLocaleDateString("en-US")}</h4>
                         <p>{t.description}</p>
                     </div>
                 ))
+
+                
             }
+            <input type="text" onChange={evt => setTreatment(evt.target.value)} placeholder="Add new treatment"/>
+            <button id="submitTreatment" onClick={()=> {
+                const newTreatment= {
+                    animalId: animal?.id,
+                    timestamp: Date.now(),
+                    description: treatment
+                }
+                AnimalRepository.addTreatment(newTreatment)
+                .then(()=>{
+                    AnimalRepository.get(animal?.id)
+                    .then(setCurrentAnimal)
+                })
+                
+                
+            }}>Submit</button>
             <button style={{
                 position: "absolute",
                 top: "1em",
